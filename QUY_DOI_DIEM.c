dff6b66eb4585e635f06d21a8c9cb4bfc40cf702
SnappyS <thinhnguyentien05@gmail.com>
2023-12-19 20:37:01 +0700

    second commit

--- QUY_DOI_DIEM.c
+++ QUY_DOI_DIEM.c
@@ -9,37 +9,40 @@
  * 
  */
 #include <stdio.h>
-
+#include <stdio.h>
+ 
 int main() {
-    float n; // Sử dụng kiểu dữ liệu float để chấp nhận giá trị có dấu thập phân
-
+    float n; // mark
+ 
     do {
         printf("Vui long nhap diem thang 10 cua ban: ");
-        scanf("%f", &n); // Sửa địa chỉ của biến n
-
-        // Kiểm tra nếu giá trị nhập vào không hợp lệ
-        if (n < 0) {
+        scanf("%f", &n);
+ 
+        if (n < 0 || n > 10 ) {
             printf("Vui long nhap lai diem cua ban\n");
         }
-    } while (n < 0);
-
-    if (n >= 0 && n < 4) {
+    } while (n < 0 || n > 10);
+ 
+    if (n >= 0 && n < 4.0) {
         printf("Thang diem 4 cua ban la 0 va duoc diem F ");
-    } else if (n >= 4 && n <= 4.9) {
+    } else if (n >= 4 && n < 5.0 ) {
         printf("Thang diem 4 cua ban la 1 va duoc diem D ");
-    } else if (n >= 5.0 && n <= 5.4) {
+    } else if (n >= 5.0 && n < 5.5) {
         printf("Thang diem 4 cua ban la 1.5 va duoc diem D+ ");
-    } else if (n >= 5.5 && n <= 6.4) {
+    } else if (n >= 5.5 && n < 6.5) {
         printf("Thang diem 4 cua ban la 2.0 va duoc diem C ");
-    } else if (n >= 6.5 && n <= 6.9) {
+    } else if (n >= 6.5 && n < 7.0) {
         printf("Thang diem 4 cua ban la 2.5 va duoc diem C+ ");
-    } else if (n >= 7.0 && n <= 7.9) {
+    } else if (n >= 7.0 && n <= 8.0) {
         printf("Thang diem 4 cua ban la 3.0 va duoc diem B ");
-    } else if (n >= 8.5 && n <= 9.4) {
+    } else if (n >= 8.0 && n < 8.5 ){
+        printf("Thang diem 4 cua ban la 3.5 va duoc diem B+ ");
+    } else if (n >= 8.5 && n < 9.5) {
         printf("Thang diem 4 cua ban la 4.0 va duoc diem A ");
     } else if (n >= 9.5 && n <= 10) {
         printf("Thang diem 4 cua ban la 4.0 va duoc diem A+ ");
     }
-
+ 
     return 0;
 }
+ 
